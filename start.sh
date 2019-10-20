bc098d9283c9de79bff075d0e342819607531182
pivovarit <gpiwowarek@gmail.com>
2019-10-20 17:36:38 +0200

    Self-sufficient start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,26 @@
 
 set -e
 
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+command_exists git || {
+		error "git is not installed"
+		exit 1
+  }
+
+git clone --depth=1 https://github.com/pivovarit/mac-os-playbook || {
+		error "git clone of oh-my-zsh repo failed"
+		exit 1
+	}
+
 if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]];
     then
-        echo "Info   | Install   | xcode"
+        echo "${BLUE}Info   | Install   | xcode${RESET}"
         xcode-select --install
     else
-        echo "Info   | OK        | xcode"
+        echo "${BLUE}Info   | OK        | xcode${RESET}"
 fi
 
 if [[ ! -x /usr/local/bin/ansible ]];
@@ -21,4 +35,4 @@ fi
 
 export PATH=/usr/local/bin:$PATH
 
-ansible-playbook playbook.yml --syntax-check && ansible-playbook playbook.yml -K
+ansible-playbook mac-os-playbook/playbook.yml --syntax-check && ansible-playbook mac-os-playbook/playbook.yml -K
