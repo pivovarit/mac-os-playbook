ece88b80fce78bb03cf462d9f648c656933a9d22
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-10-20 20:10:25 +0200

    Start refactor (#52)
    * Refactor start.sh
    
    * Refactor start.sh
    
    * Refactor start.sh
    
    * Refactor start.sh

--- start.sh
+++ start.sh
@@ -2,39 +2,59 @@
 
 set -e
 
-command_exists() {
+error() {
+	echo "${RED}""Error: $@""${RESET}" >&2
+	exit 1
+}
+
+ok() {
+	echo "${GREEN}""Info   | OK        | $@""${RESET}"
+}
+
+installing() {
+	echo "${BLUE}""Info   | Install   | $@""${RESET}"
+}
+
+exists() {
 	command -v "$@" >/dev/null 2>&1
 }
 
-command_exists git || {
-		error "git is not installed"
-		exit 1
-  }
+if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		BLUE=$(printf '\033[34m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		BLUE=""
+		RESET=""
+fi
 
-git clone --depth=1 https://github.com/pivovarit/mac-os-playbook || {
-		error "git clone of oh-my-zsh repo failed"
-		exit 1
-	}
+exists git || error "git is not installed"
+
+TARGET="$(pwd)"
+
+if [ "x$1" != "x--local" ]
+    then
+      git clone -q --depth=1 https://github.com/pivovarit/mac-os-playbook || error "git clone of oh-my-zsh repo failed, run with --local if already cloned"
+	    TARGET='mac-os-playbook/'
+fi
 
 if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]];
     then
-        echo "Info   | Install   | xcode"
-        xcode-select --install
+        installing "xcode" && xcode-select --install
     else
-        echo "Info   | OK        | xcode"
+        ok "xcode"
 fi
 
 if [[ ! -x /usr/local/bin/ansible ]];
     then
-        echo "Info   | Install   | ansible"
-        brew update
-        brew install ansible
+        installing "ansible" && brew update && brew install ansible
     else
-        echo "Info   | OK        | ansible"
+        ok "ansible"
 fi
 
 export PATH=/usr/local/bin:$PATH
 
-cd mac-os-playbook
-
-ansible-playbook playbook.yml -K
+cd $TARGET && ansible-playbook playbook.yml -K
